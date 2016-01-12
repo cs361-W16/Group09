e8acdaff3d26b330301f4903681c20aac4cb322d
Michael Hilton <michaelhilton@10-249-90-26.wireless.oregonstate.edu>
2016-01-12 12:59:37 -0800

    Added tests to bring it to 100% coverage for Controller

--- Assignment1/src/test/java/controllers/ApiControllerDocTesterTest.java
+++ Assignment1/src/test/java/controllers/ApiControllerDocTesterTest.java
@@ -29,34 +29,27 @@ import static org.junit.Assert.assertThat;
 public class ApiControllerDocTesterTest extends NinjaDocTester {
     
     String URL_INDEX = "/";
-    String URL_HELLO_WORLD_JSON = "/hello_world.json";
+    String URL_ACES_UP = "/AcesUp";
     
-//    @Test
-//    public void testGetIndex() {
-//
-//        Response response = makeRequest(
-//                Request.GET().url(
-//                        testServerUrl().path(URL_INDEX)));
-//
-//        assertThat(response.payload, containsString("Hello World!"));
-//        assertThat(response.payload, containsString("BAM!"));
-//
-//
-//    }
-//
-//    @Test
-//    public void testGetHelloWorldJson() {
-//
-//        Response response = makeRequest(
-//                Request.GET().url(
-//                        testServerUrl().path(URL_HELLO_WORLD_JSON)));
-//
-//        ApplicationController.SimplePojo simplePojo
-//                = response.payloadJsonAs(ApplicationController.SimplePojo.class);
-//
-//        assertThat(simplePojo.content, CoreMatchers.equalTo("Hello World! Hello Json!"));
-//
-//
-//    }
+    @Test
+    public void testGetIndex() {
+
+        Response response = makeRequest(
+                Request.GET().url(
+                        testServerUrl().path(URL_INDEX)));
+
+        assertThat(response.payload, containsString("Hello World!"));
+        assertThat(response.payload, containsString("BAM!"));
+    }
+
+    @Test
+    public void testAcesUp() {
+        Response response = makeRequest(
+                Request.GET().url(
+                        testServerUrl().path(URL_ACES_UP)));
+
+        assertThat(response.payload, containsString("Aces Up"));
+        assertThat(response.payload, containsString("columnOfCards"));
+    }
 
 }
