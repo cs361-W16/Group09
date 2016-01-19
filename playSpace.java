534e5b0017a72917b5daa60e55bc765e7c1d6379
Michael Hilton <michael.hilton@gmail.com>
2016-01-19 15:24:11 -0800

    Revert "Setting up basic playSpace"

--- playSpace.java
+++
@@ -1,9 +0,0 @@
-
-public class playSpace{
-
-    public Card[][] array = new Card[4][13]; //4 rows, each has the potential to hold 13 cards in a worst-case game
-
-    assertequals(0, array[0][0].getValue());
-
-
-}
