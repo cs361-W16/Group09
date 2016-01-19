3a6bc6bc7cfe23d50a540ddc5836bd28934934dc
bro1017 <dylanw3000@gmail.com>
2016-01-19 15:22:18 -0800

    Setting up basic playSpace
    I have no idea if this works because idk how to get it into the project

---
+++ playSpace.java
@@ -0,0 +1,9 @@
+
+public class playSpace{
+
+    public Card[][] array = new Card[4][13]; //4 rows, each has the potential to hold 13 cards in a worst-case game
+
+    assertequals(0, array[0][0].getValue());
+
+
+}
