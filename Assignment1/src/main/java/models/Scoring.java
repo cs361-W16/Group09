f255e7c9373e32c040b008d2d5215e5b6249ff8a
Paul Lantow <paul.lantow@gmail.com>
2016-01-21 14:08:06 -0800

    Fleshed out the detection system.

--- Assignment1/src/main/java/models/Scoring.java
+++ Assignment1/src/main/java/models/Scoring.java
@@ -1,9 +1,22 @@
 package models;
 
 /**
- * Created by paullantow on 1/20/16.
+ * Created by TheTallPaul on 1/20/16.
  */
 
 public class Scoring {
-    
+    public Scoring() {
+        //Run through the columns
+        for (int i = 0; i < 3; i++) {
+            //Run through the rows
+            for (int j = 0; j < 12; j++) {
+                String top = getCard(i,j);
+                String bottom = getCard(i,j+1);
+                //Check for suit and that the top is greater by 1
+                if (top[2] == bottom[2] && Integer.parseInt(top.substring(0,1)) == Integer.parseInt(bottom.substring(0,1))+1) {
+
+                }
+            }
+        }
+    }
 }
