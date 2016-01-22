dfe0a1f85e860305f8f232d4b35031dbb5079993
Touku Cha <chat@oregonstate.edu>
2016-01-21 18:24:49 -0800

    testing sourcetree and adding in my eventual feature of drawing four cards.

---
+++ Assignment1/src/main/java/models/DrawFour.java
@@ -0,0 +1,18 @@
+package models;
+
+/**
+ * Created by touku_000 on 1/21/2016.
+ */
+
+import java.util.Collections;
+import java.util.Arrays;
+import java.util.List;
+
+public class DrawFour {
+	
+	//grab current deck count
+	
+	//is it possible to take up to 4 cards out of the deck
+	
+	//
+}
---
+++ Assignment1/src/test/java/models/DrawFourTest.java
@@ -0,0 +1,17 @@
+package models;
+import org.junit.Test;
+import static org.junit.Assert.*;
+/**
+ * Created by touku_000 on 1/21/2016.
+ */
+ 
+ 
+public class DrawFourTest {
+	
+	@Test
+	public void Draw1() {
+		
+	}
+
+	
+}
