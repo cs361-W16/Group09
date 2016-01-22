04144f086bd03837d8efaf9f755a6231e5b88c9b
Paul Lantow <paul.lantow@gmail.com>
2016-01-21 18:37:49 -0800

    Added Board class, that keeps track of the board state.

--- Assignment1/src/main/java/models/Scoring.java
+++ Assignment1/src/main/java/models/Scoring.java
@@ -12,12 +12,12 @@ public class Scoring {
         for (int i = 0; i < 3; i++) {
             //Run through the rows
             for (int j = 0; j < 12; j++) {
-                String top = getCard(i,j);
-                String bottom = getCard(i,j+1);
+                String top = getCard(j,i);
+                String bottom = getCard(j+1,i);
                 //Check for suit and that the top is greater by 1
                 if (top[2] == bottom[2] && Integer.parseInt(top.substring(0,1)) == Integer.parseInt(bottom.substring(0,1))+1) {
                     //Remove card
-                    modBoard(i,j+1,0);
+                    modBoard(j+1,i,0);
                     //Increment score
                     incScore();
                 }
