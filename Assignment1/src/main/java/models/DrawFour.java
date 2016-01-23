9a1947a052a141f32bd7ba663966e13a66da32aa
toukucha <chat@oregonstate.edu>
2016-01-22 16:57:23 -0800

    no message

--- Assignment1/src/main/java/models/DrawFour.java
+++ Assignment1/src/main/java/models/DrawFour.java
@@ -10,10 +10,20 @@ import java.util.List;
 
 public class DrawFour {
 
-    public int cardDeck = 52;
+    private int cardDeck = 52;
 
     public int Draw(int drawed){
         if(cardDeck != 0) {
+
+            //initial array, oldArray should be replaced by the Deck array
+            int[] oldArray = {1,2,3,4,5};
+
+            //define a new array + allocate space for four cards
+            int[] drawArray = new int[4];
+
+            //copy values
+            System.arraycopy(oldArray, 0, drawArray, 0, oldArray.length);
+
             cardDeck -= 4;
             //test print
             System.out.println("You drew four card and have:" + cardDeck + " cards left in the deck");
